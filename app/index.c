764ad72a3e286ba96b7451f802f93e368c9419c6
BowersIndustry <Bowersindustry.bwrs@gmail.com>
2022-06-23 22:39:40 -0500

    No idea

---
+++ app/index.c
@@ -0,0 +1,23 @@
+GBA game engine.
+How about...
+
+GBA Studio?
+No...
+
+GBAMaker?
+Nah.
+
+Oh! I got it!
+GBA Creator!
+
+
+It's the second piece of software in the "Creators".
+
+1. DS Creator
+2. GBA Creator
+3. #i$ Creator
+4. %&@U Creator
+5. #%$&e%&# $#%t&% Creator
+
+Now, are all of these wanted? Yes.
+Will they all be made? Only time will tell...
