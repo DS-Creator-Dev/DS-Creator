e1f7a932c09a269d321e6f4385dc5e7cdcb9924d
NotImplementedLife <not.implemented.life@gmail.com>
2022-09-05 19:49:31 +0300

    engine: resource handling abstractization

---
+++ engine/include/DSCEngine/resources/readonly_data.hpp
@@ -0,0 +1,71 @@
+#pragma once
+
+namespace DSC
+{
+	/// ReadOnlyData denotes a type of static resource 
+	/// that lies either in RAM, FAT or NITRO.
+	/// 
+	/// This class represents an abstract interface for resource management 
+	/// and should be able to retrieve any bytes buffer regardless of its 
+	/// localization.
+	///
+	/// This class can also be derived to describe more specialized resource
+	/// formats.
+	///
+	/// This class is NOT the binary data itself, but only an expression of it.
+	/// It is more like a header that resides only in WRAM, whilst the data is
+	/// points to can be anywhere.
+	///
+	/// Instances of this class should be constants, not allow moving/making copies of them.
+	/// In fact, only DSC specialized tools have to create reliable ReadOnlyData instances.
+	///
+	/// Referencing instances of this class should be done only through pointers
+	/// (attention to down/upcasts, no polymorphism is being used)
+	///
+	///
+	/// [ !!! POSSIBLE BUG SOURCE !!! ] There is no "Read-Only Memory" thing on DS homebrew. 
+	/// All the constants are being placed in EWRAM (4MB). The compiler tries its best to
+	/// prevent programmers from altering constant values, but one can simply break the rule
+	/// by overwriting a constant only using its dereferenced pointer. Assuming the programmer doesn't
+	/// try anything strange, there is still a chance that an undefined behavior/RAM corruption/
+	/// some accidental buffer overflow (eg. for i=0;i<10;i--) may overwrite in-memory "read-only" data.
+	/// We should keep a careful eye on that and consider it as a potential explanation for any visual glitches
+	/// or misterious data corruption. Therefore, when constant data changes its bytes, then something's really messed up.
+	
+	class ReadOnlyData
+	{		
+	public:                      /// Some technical observations to account for during implementation:
+	
+		int header_size;         /// equilavent of sizeof(*this), but foresees class inheritance
+		int data_length;         /// size in bytes of the actual data
+		bool is_file;            /// tell if the data is in RAM or on a filesystem
+		char* data_source;       /// if is_file==true, data_source is the file path (fat://..., nitro://...)
+		                         /// if is_file==false, data_source is a pointer to WRAM
+						         /// if data_source==NULL, then the actual data resides right after *this
+		bool compression_type;   /// 0 = no compression, 1=LZ7 or sth, ...
+		
+		ReadOnlyData(int header_size = sizeof(ReadOnlyData));		
+		
+		// writes binary data to the given address (most popular expected use: VRAM)		
+		void extract(void* destination); 
+		
+		// prevent altering this object
+		ReadOnlyData(const ReadOnlyData&) = delete;
+		ReadOnlyData(ReadOnlyData&&) = delete;
+		ReadOnlyData operator = (const ReadOnlyData&) = delete;
+		ReadOnlyData operator = (ReadOnlyData&&) = delete;		
+	};
+	
+	class AssetData : public ReadOnlyData // only as an example
+	{
+	public:
+		int width;        // new properties
+		int height;       // packed in a single place 
+		bool is_bitmap;
+		int color_depth;
+		//...
+		
+		AssetData() : ReadOnlyData(sizeof(AssetData)) {} // this line is important
+		~AssetData() = delete;
+	};
+}
---
+++ engine/source/DSCEngine/resources/readonly_data.cpp
@@ -0,0 +1,26 @@
+#include "DSCEngine/resources/readonly_data.hpp"
+
+DSC::ReadOnlyData::ReadOnlyData(int header_size) : header_size(header_size) { }
+
+void DSC::ReadOnlyData::extract(void* destination)
+{
+	if(!is_file)
+	{		
+		// perform 16-bit r/w
+		short* src = reinterpret_cast<short*>(data_source);
+		if(src == nullptr)
+		{
+			src = (short*)(((int)this)+header_size);
+		}		
+		
+		short* dst = reinterpret_cast<short*>(destination);
+		for(int i=0;i<data_length/2;i++)
+			dst[i] = src[i]; // maybe __asm this line if gcc optimizes it to memset (<=> byte-to-byte copy)
+	}
+	else
+	{
+		char* filename = data_source;
+		
+		// discussion FAT/NITRO...
+	}
+}
